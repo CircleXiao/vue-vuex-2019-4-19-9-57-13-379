529338c5917e8e4ce9c6f711c202d19f91be844e
mmy <mmy_xize@163.com>
2019-09-03 20:42:12 +0800

    展示todolist页面，并添加样式

---
+++ vuex-todolist/src/components/todoList.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="todolist">
+    <h2>Vuex todo list</h2>
+    <p>Simple Todo List with adding and filter by diff status.</p>
+
+    <input type="text" class="input-text" name="listItem" placeholder="input your plan..." />
+    <div class="input-btn">Add</div>
+    <br />
+
+    <ol>
+      <li v-for="(item, index) in todoList" :key="index">
+        <span>{{ index + 1 }}.</span>
+        <input type="checkbox" name="done-todo" class="done-todo" />
+        <span>{{ item }}</span>
+      </li>
+    </ol>
+
+    <ul>
+      <li v-for="btnItem in btnGroup" :key="btnItem">
+        <a href="#" data-filter="btnItem.itemFilter" class="selected">{{ btnItem.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {},
+  data() {
+    return {
+      todoList: ["sing", "play", "888888888888888"],
+      btnGroup: [
+        {
+          itemFilter: "all",
+          name: "All"
+        },
+        {
+          itemFilter: "active",
+          name: "Active"
+        },
+        {
+          itemFilter: "complete",
+          name: "Complete"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.todolist {
+  width: 600px;
+  height: auto;
+  margin: 50px auto;
+  border-radius: 6px;
+  background-color: antiquewhite;
+}
+h2 {
+  font-size: 1.8em;
+  font-weight: bolder;
+  padding-top: 20px;
+}
+p {
+  font-style: italic;
+}
+.input-text {
+  width: 60%;
+  padding: 6px 16px;
+  margin-right: 10px;
+}
+.input-btn {
+  display: inline-block;
+  width: 30px;
+  background-color: #fc999b;
+  color: #ffffff;
+  padding: 6px 16px;
+}
+ol{
+    text-align: left;
+    margin-left: 20px;
+}
+ol li{
+    display: block;
+    margin-bottom: 6px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 16px;
+}
+ul li a {
+  color: #42b983;
+  text-decoration: none;
+  width: 20px;
+  padding: 2px 16px;
+}
+ul li a.selected{
+    border: 1px solid #42b983;
+}
+</style>
